90bf8b0583be356cc6cabd89931b8def32d80a97
Swagnick99 <sagnikbasu19@gmail.com>
2021-07-05 21:12:59 +0530

    APIs added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent} from './home/home.component';
+import { AdminComponent} from './admin/admin.component';
+import { LabComponent} from './lab/lab.component';
+import { TestsComponent} from './tests/tests.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',component:HomeComponent
+  },
+  {
+    path:'admin',component:AdminComponent
+  },
+  {
+    path:'lab',component:LabComponent
+  },
+  {
+    path:'test',component:TestsComponent
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { LabComponent } from './lab/lab.component';
+import { TestsComponent } from './tests/tests.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    LabComponent,
+    TestsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
